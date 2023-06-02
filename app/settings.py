4fb555dee56286864ad0b8135fd4e627e4d2e2ea
higorpr <higorpr@gmail.com>
2023-06-02 14:58:12 -0300

    feat: improve security

--- app/settings.py
+++ app/settings.py
@@ -6,4 +6,4 @@ MONGO_URI = os.getenv("MONGO_URI")
 class TestingConfig(Config):
     DEBUG=False
     TESTING = True
-    MONGO_URI = "mongodb+srv://higorpr:0u0e657A@starwarscluster.tf1zbiq.mongodb.net/StarWarsDatabase?retryWrites=true&w=majority"
+    MONGO_URI = os.getenv("MONGO_URI")
